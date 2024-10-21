2517aae1b05cdda4bea7823d094475a4a372de83
DarthArth <dylanarathgonzalezrenteria@gmail.com>
2024-10-21 09:32:47 -0600

    Add files via upload

---
+++ P00Precentacion.php
@@ -0,0 +1,12 @@
+<?php
+/* CBTIS 89
+   Programa que imprime mis datos
+   Dylan Arath Gonzalez Renteria
+   3roA Programacion Matutino
+*/
+   echo "** MIS DATOS **","<br>","<br>";
+   echo "Mi nombre es Dylan Arath Gonzalez Renteria", "<br>";
+   echo "Estoy en el CBTIS 89 en la carrera de Programacion 3roA Matutino","<br>";
+   echo "Vivo en la ciudad de Durango, Dgo";
+
+?>
